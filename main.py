644101f554717a3f1ba4f2128e80128c381f86df
Arybjan <arybjen92@gmail.com>
2023-01-24 15:27:25 +0600

    first commit, add a little settings for apple

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+from random import randrange 
+
+RES = 800
+SIZE = 50
+
+x, y = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+apple = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+
